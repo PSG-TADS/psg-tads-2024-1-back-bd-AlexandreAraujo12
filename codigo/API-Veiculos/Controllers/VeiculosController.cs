0051a12141f60bd48e7c2016a887a44cd7df97b7
AlexandreAraujo12 <dagoberto633@gmail.com>
2024-04-20 19:14:24 -0300

    criacao de API ASPNetCore para interagir com banco de dados relacional do SQL Express

---
+++ codigo/API-Veiculos/Controllers/VeiculosController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API_Veiculos.Models;
+
+namespace API_Veiculos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VeiculosController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+
+        public VeiculosController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Veiculos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Veiculo>>> GetVeiculo()
+        {
+            return await _context.Veiculo.ToListAsync();
+        }
+
+        // GET: api/Veiculos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Veiculo>> GetVeiculo(int id)
+        {
+            var veiculo = await _context.Veiculo.FindAsync(id);
+
+            if (veiculo == null)
+            {
+                return NotFound();
+            }
+
+            return veiculo;
+        }
+
+        // PUT: api/Veiculos/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVeiculo(int id, Veiculo veiculo)
+        {
+            if (id != veiculo.IdVeiculo)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(veiculo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VeiculoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Veiculos
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Veiculo>> PostVeiculo(Veiculo veiculo)
+        {
+            _context.Veiculo.Add(veiculo);
+            await _context.SaveChangesAsync();
+
+            //return CreatedAtAction("GetVeiculo", new { id = veiculo.IdVeiculo }, veiculo);
+            return CreatedAtAction(nameof(GetVeiculo), new { id = veiculo.IdVeiculo }, veiculo);
+        }
+
+        // DELETE: api/Veiculos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVeiculo(int id)
+        {
+            var veiculo = await _context.Veiculo.FindAsync(id);
+            if (veiculo == null)
+            {
+                return NotFound();
+            }
+
+            _context.Veiculo.Remove(veiculo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool VeiculoExists(int id)
+        {
+            return _context.Veiculo.Any(e => e.IdVeiculo == id);
+        }
+    }
+}
---
+++ codigo/API-Veiculos/Models/Veiculo.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_Veiculos.Models
+{
+    public class Veiculo
+    {
+        [Key]
+        public int IdVeiculo { get; set; }
+        public string? Marca { get; set; }
+        public string? Modelo { get; set; }
+        public string? Status { get; set; }
+        public ICollection<Veiculo>? Veiculos { get; set; }
+
+    }
+}
